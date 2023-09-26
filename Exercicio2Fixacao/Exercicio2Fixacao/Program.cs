c067afe20145bb534a9d64561fe933ec9d1e6373
vascsx <vasczn@gmail.com>
2023-09-26 20:18:09 -0300

    Exercicio Fixação 2 de estrutura de condicao - UDEMY C# POO

---
+++ Exercicio2Fixacao/Exercicio2Fixacao/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+using System;
+namespace YourNamespace
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} é par");
+            }
+            else if (number % 2 == 1)
+            {
+                Console.WriteLine($"{number} é impar");
+            }
+            else {
+                Console.WriteLine("Numero Invalido");
+            }
+        }
+    }
+}
