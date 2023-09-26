bf637e1bb7832a346ac9dedf02e799860a0cd345
vascsx <vasczn@gmail.com>
2023-09-26 17:08:37 -0300

    repo de estudos de C# POO

---
+++ ExercicioFixacao_EntradaDeDados/ExercicioFixacao_EntradaDeDados/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+string frase = Console.ReadLine();
+    string x = Console.ReadLine();
+    string y = Console.ReadLine();
+    string z = Console.ReadLine();
+
+    string s = Console.ReadLine();
+    string[] vet = s.Split(' ');
+    string p1 = vet[0];
+    string p2 = vet[1];
+    string p3 = vet[2];
+Console.Write("Você digitou: ");
+Console.WriteLine(frase);
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.WriteLine(p3);
---
+++ PrimeiroProjeto/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
