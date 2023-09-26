a13df68f284a3c746edc75cb1526922bd9d00a7a
vascsx <vasczn@gmail.com>
2023-09-26 20:09:46 -0300

    adicionando primeiro exercicio de condicional - CURSO UDEMY POO

---
+++ ExercicioFix1/ExercicioFix1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace YourNamespace
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero para saber se é negativo ou positivo: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 0)
+            {
+                Console.WriteLine($"o {number} é positivo.");
+            } else if (number < 0)
+            {
+                Console.WriteLine($"o {number} é negativo");
+            }else
+            {
+                Console.WriteLine("Numero Invalido");
+            }
+        
+        
+        }
+    }
+}
