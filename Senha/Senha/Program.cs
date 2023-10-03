00e495492eeac832eb66762812760e6cbc0d088b
vascsx <vasczn@gmail.com>
2023-10-03 17:44:44 -0300

    Exercicio01 estrutura condicional while

---
+++ Senha/Senha/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Program
+{
+    class Senha
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Acesso não Permitido!");
+                Console.Write("Digite um numero: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido!");
+        }
+    }
+}
