867d8b55ce1510e484f72de6f19fc480048add28
vascsx <vasczn@gmail.com>
2023-10-04 11:52:05 -0300

    Ex02 POO

---
+++ Idade_POO/Idade_POO/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade_POO
+{
+    class Pessoa
+    {
+        public string A;
+        public string B;
+        public int C;
+        public int D;
+    }
+
+}
---
+++ Idade_POO/Idade_POO/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Idade_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa x, y,z,h;
+            x = new Pessoa();
+            y = new Pessoa();
+
+            Console.Write("Digite o nome da primeira pessoa: ");
+            x.A = Console.ReadLine();
+            Console.Write("Digite o idade da primeira pessoa: ");
+            y.C = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome da segunda pessoa: ");
+            x.B = Console.ReadLine();
+            Console.Write("Digite o idade da segunda pessoa: ");
+            y.D = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.WriteLine("Nome: " + x.A);
+            Console.WriteLine("Idade: " + y.C);
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.WriteLine("Nome: " + x.B);
+            Console.WriteLine("Idade: " + y.D);
+
+            if (y.C > y.D)
+            {
+                Console.WriteLine("Pessoa mais velha: " + x.A);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + x.B);
+
+            }
+        }
+    }
+}
