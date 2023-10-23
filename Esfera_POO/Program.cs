7f8de5130911e453408fec128084e3c03f31f280
vascsx <vasczn@gmail.com>
2023-10-23 17:27:13 -0300

    Esfera POO - udemy C# POO

---
+++ Esfera_POO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Esfera_POO
+{
+    class Esfera_POO
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Entre com valor do raio: ");
+            E.raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("CIRCUFERENCIA: " + E.Circuferencia().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + E.Volume().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PI: " + E.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
---
+++ Esfera_POO/calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Esfera_POO
+{
+    internal class Calculadora
+    {
+        public static double raio;
+        public static double Pi = 3.14;
+
+        public static double Circuferencia() { 
+           return 2 * Pi * raio;
+        }
+
+        public static double Volume()
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3.0);
+        }
+    }
+}
