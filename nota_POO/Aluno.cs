dd2e1e44673207de1e20265b4bb2db4c673faf2a
vascsx <vasczn@gmail.com>
2023-10-23 16:46:54 -0300

    Exercicio de POO Notas - Udemy C# POO

---
+++ nota_POO/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nota_POO
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double Media()
+        {
+            return Nota1+Nota2+Nota3;
+        }
+
+
+        public double NotaFalta()
+        {
+            return 60 - Media();
+        }
+    }
+}
---
+++ nota_POO/Program.cs
@@ -0,0 +1,43 @@
+﻿//Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+//(primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
+//ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam
+//para o aluno obter o mínimo para ser aprovado (que é 60 pontos). Você deve criar uma classe Aluno para resolver
+//este problema.//
+
+
+
+using System;
+using System.Globalization;
+
+namespace nota_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno A = new Aluno();
+            Console.Write("Nome do aluno: ");
+            A.Nome = Console.ReadLine();
+            Console.Write("Primeira nota: ");
+            A.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Segunda nota: ");
+            A.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Terceira nota: ");
+            A.Nota3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+      
+
+
+            Console.WriteLine("MEDIA: " + A.Media().ToString("F2", CultureInfo.InvariantCulture));
+
+            if ( A.Media() >= 60)
+            {
+                Console.WriteLine("APROVADO");
+            } 
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM: " + A.NotaFalta().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
