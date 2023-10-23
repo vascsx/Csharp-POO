07d587304fee88d1ba04063504cb0c7bffad686c
vascsx <vasczn@gmail.com>
2023-10-22 23:40:08 -0300

    Exercicio de Fixação - Udemy C# POO

---
+++ Empresa/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Empresa
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem){
+            SalarioBruto = (SalarioBruto * porcentagem / 100.00) + SalarioBruto;
+            }
+
+        public override string ToString()
+        {
+            return Nome + ", R$ " +
+                  SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);  
+        }
+    }
+}
---
+++ Empresa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Empresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.Write("Funcionario: " + func);
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salario: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine("");
+            Console.Write("Dados Atualizados: " + func);
+
+        }
+    }
+}
