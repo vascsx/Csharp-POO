3f059a5af73562401ddc1f4f5271d3259ca1bcbd
vascsx <vasczn@gmail.com>
2023-10-22 23:07:05 -0300

    Exercicio Fixação de POO - Udemy Curso POO (Classes)

---
+++ TrianguloEx_poo/TrianguloEx_poo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace TrianguloEx_poo
+{
+
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        Retangulo r = new Retangulo();
+        Console.WriteLine("Digite a altura e largura do retangulo: ");
+        r.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        r.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("AREA: " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("PERIMETRO: " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("DIAGONAL: " + r.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+    }
+
+    }
+}
---
+++ TrianguloEx_poo/TrianguloEx_poo/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloEx_poo
+{
+    internal class Retangulo
+    {
+        public double altura;
+        public double largura;
+ 
+        public double Area()
+        {
+           return largura * altura;
+        }
+
+        public double Perimetro()
+        {
+            return (largura * 2) + (altura * 2);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(largura*largura + altura * altura);
+        }
+    }
+}
