03d5f58cec32c912cc2ecb5ac6b6dcf19710e774
vascsx <vasczn@gmail.com>
2023-12-16 18:45:17 -0300

    fazendo desafio de encapsulamento, properties, sobrecarga e construtores.

---
+++ DesafioPOO - Conta Bancaria/Program.cs
@@ -0,0 +1,45 @@
+﻿using DesafioPOO___Conta_Bancaria;
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa pessoa = new Pessoa();
+
+        Console.Write("Entre com o número dda conta: ");
+        pessoa.Id = int.Parse(Console.ReadLine());
+
+        Console.Write("Entre o titular da conta: ");
+        pessoa.Nome = (Console.ReadLine());
+
+        Console.Write("Haverá depósito inicial (s/n) ? ");
+        pessoa.Escolha = (Console.ReadLine());
+
+        if (pessoa.Escolha == "s")
+        {
+            Console.Write("Entre com um valor de depósito: ");
+            pessoa.Deposito = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados da Conta: " + "Conta: " + pessoa.Id + "," + " Titular: " + pessoa.Nome + "," + " Saldo: " + pessoa.Deposito);
+        }
+
+        else if ( pessoa.Escolha == "n") 
+        {
+            Console.WriteLine("Dados da Conta: " + "Conta: " + pessoa.Id + "," + " Titular: " + pessoa.Nome + "," + " Saldo: " + pessoa.Deposito);
+        }
+
+        else
+        {
+            Console.WriteLine("Erro tente novamente: ");
+            Console.Write("Haverá depósito inicial (s/n) ? ");
+            pessoa.Escolha = (Console.ReadLine());
+        }
+
+        Console.Write("Entre com um valor para saque: ");
+        pessoa.Saque = double.Parse(Console.ReadLine());
+        pessoa.Deposito = pessoa.Deposito - pessoa.Saque;
+        Console.WriteLine("Dados da Conta atualizados: \n" + "Conta: " + pessoa.Id + "," + " Titular: " + pessoa.Nome + "," + " Saldo: " + pessoa.Deposito);
+
+    }
+}
---
+++ DesafioPOO - Conta Bancaria/contaBancaria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPOO___Conta_Bancaria
+{
+    internal class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Escolha;
+
+        public double Deposito;
+
+        public double Saque;
+
+
+
+
+    }
+}
