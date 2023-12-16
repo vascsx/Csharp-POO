598b21689a87ecc73068920e7207b88126619d54
vascsx <vasczn@gmail.com>
2023-12-16 10:32:51 -0300

    estudos de sobrecarga C# POO

---
+++ Produto/Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5;
+        }
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $" +
+                ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Produto/Program.cs
@@ -0,0 +1,43 @@
+﻿using Product;
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Program { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantida no estoque: ");
+
+            Produto p = new Produto(nome,preco);
+            Produto p2 = new Produto();
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado: ");
+            int qte = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido: ");
+            qte = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.RemoverProdutos(qte);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    
+    }
+}
