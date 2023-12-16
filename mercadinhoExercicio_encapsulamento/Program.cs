95bd9b1bcb4b381eec8b3e0a4a54cb011174ec56
vascsx <vasczn@gmail.com>
2023-12-16 14:37:52 -0300

    estudo de encapsulamento C# POO

---
+++ mercadinhoExercicio_encapsulamento/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 20);
+            p.SetNome("Vasc Programador .NET MAUI");
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+        }
+    }
+}
